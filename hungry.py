13514a2848189ce6bc23fbbfc66f3a89ec850ca3
vedsharma007 <ved.s@credright.com>
2024-11-15 16:44:57 +0530

    removed content from humgry.py file

--- hungry.py
+++ hungry.py
@@ -2,10 +2,7 @@ hungry = input('are you hungry')
 if hungry == 'yes':
     print('eat samosa')
 <<<<<<< HEAD
-    print('eat pizza')
-    print('eat burger')
-    print('eat fries')
-    print('eat dryfruits')
+
 =======
 >>>>>>> d0c4b26afcc70285f1da7b2a36574d29d884e1ab
 else:
